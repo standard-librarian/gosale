0ddb31b720cffb8bfe14d6ef7a2528b620b17433
standard-librarian <medhatmohammed1612@gmail.com>
2025-12-13 10:09:27 +0200

    Log startup and shutdown, output config
    Signed-off-by: standard-librarian <medhatmohammed1612@gmail.com>

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -75,6 +75,17 @@ func run(ctx context.Context, log *logger.Logger) error {
 		}
 		return fmt.Errorf("parsing config: %w", err)
 	}
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
 
 	// -------------------------------------------------------------------------
 
