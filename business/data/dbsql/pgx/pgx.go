7a8644120527df68e7aaaf24078b1a932da8c87f
standard-librarian <medhatmohammed1612@gmail.com>
2025-12-21 20:31:23 +0200

    Add DB migrations, seed, pgx helpers and dbarray
    Port dbarray implementation from lib/pq. Add business/data/dbmigrate
    package with Migrate and Seed and accompanying SQL migration and seed
    files. Add pgx DB helpers: Open, StatusCheck, NamedExecContext and
    common error mappings.

--- business/data/dbsql/pgx/pgx.go
+++ business/data/dbsql/pgx/pgx.go
@@ -1,1 +1,159 @@
+// Package db provides support for access the database.
 package db
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/standard-librarian/gosale/foundation/logger"
+)
+
+const (
+	uniqueViolation = "23505"
+	undefinedTable  = "42P01"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrDBNotFound        = sql.ErrNoRows
+	ErrDBDuplicatedEntry = errors.New("duplicated entry")
+	ErrUndefinedTable    = errors.New("undefined table")
+)
+
+// Config is the required properties to use the database.
+type Config struct {
+	User         string
+	Password     string
+	Host         string
+	Name         string
+	Schema       string
+	MaxIdleConns int
+	MaxOpenConns int
+	DisableTLS   bool
+}
+
+// Open knows how to open a database connection based on the configuration.
+func Open(cfg Config) (*sqlx.DB, error) {
+	sslMode := "require"
+	if cfg.DisableTLS {
+		sslMode = "disable"
+	}
+
+	q := make(url.Values)
+	q.Set("sslmode", sslMode)
+	q.Set("timezone", "utc")
+	if cfg.Schema != "" {
+		q.Set("search_path", cfg.Schema)
+	}
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(cfg.User, cfg.Password),
+		Host:     cfg.Host,
+		Path:     cfg.Name,
+		RawQuery: q.Encode(),
+	}
+
+	db, err := sqlx.Open("pgx", u.String())
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+
+	return db, nil
+}
+
+// StatusCheck returns nil if it can successfully talk to the database. It
+// returns a non-nil error otherwise.
+func StatusCheck(ctx context.Context, db *sqlx.DB) error {
+	if _, ok := ctx.Deadline(); !ok {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, time.Second)
+		defer cancel()
+	}
+
+	var pingError error
+	for attempts := 1; ; attempts++ {
+		pingError = db.Ping()
+		if pingError == nil {
+			break
+		}
+		time.Sleep(time.Duration(attempts) * 100 * time.Millisecond)
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+	}
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	// Run a simple query to determine connectivity.
+	// Running this query forces a round trip through the database.
+	const q = `SELECT true`
+	var tmp bool
+	return db.QueryRowContext(ctx, q).Scan(&tmp)
+}
+
+// NamedExecContext is a helper function to execute a CUD operation with
+// logging and tracing where field replacement is necessary.
+func NamedExecContext(ctx context.Context, log *logger.Logger, db sqlx.ExtContext, query string, data any) error {
+	q := queryString(query, data)
+
+	if _, ok := data.(struct{}); ok {
+		log.Infoc(ctx, 5, "database.NamedExecContext", "query", q)
+	} else {
+		log.Infoc(ctx, 4, "database.NamedExecContext", "query", q)
+	}
+
+	if _, err := sqlx.NamedExecContext(ctx, db, query, data); err != nil {
+		if pqerr, ok := err.(*pgconn.PgError); ok {
+			switch pqerr.Code {
+			case undefinedTable:
+				return ErrUndefinedTable
+			case uniqueViolation:
+				return ErrDBDuplicatedEntry
+			}
+		}
+		return err
+	}
+
+	return nil
+}
+
+// =============================================================================
+
+// queryString provides a pretty print version of the query and parameters.
+func queryString(query string, args any) string {
+	query, params, err := sqlx.Named(query, args)
+	if err != nil {
+		return err.Error()
+	}
+
+	for _, param := range params {
+		var value string
+		switch v := param.(type) {
+		case string:
+			value = fmt.Sprintf("'%s'", v)
+		case []byte:
+			value = fmt.Sprintf("'%s'", string(v))
+		default:
+			value = fmt.Sprintf("%v", v)
+		}
+		query = strings.Replace(query, "?", value, 1)
+	}
+
+	query = strings.ReplaceAll(query, "\t", "")
+	query = strings.ReplaceAll(query, "\n", " ")
+
+	return strings.Trim(query, " ")
+}
